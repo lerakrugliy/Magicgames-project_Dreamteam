44b20dac1c2bb0ec81493f789b75ddee07b0e8e1
lerakrugliy <lerakrugliy@gmail.com>
2026-07-16 01:33:38 +0300

    fix styles and add header modal

--- src/js/check_year.js
+++ src/js/check_year.js
@@ -3,15 +3,17 @@ const inputEl = document.querySelector('.check_year__input');
 const resultEl = document.querySelector('.check_year__result');
 
 butEl.addEventListener('click', () => {
-    if (isNaN(inputEl.value) || inputEl.value === '') {
-        resultEl.innerHTML = 'Ви не ввели число';
-        resultEl.classList.remove('success');
-        resultEl.classList.add('error');
-    }
-    else {
-        resultEl.innerHTML = inputEl.value % 4 === 0 ? 'Ви народилися у високосний рік!' : 'Ви народилися не у високосний рік!';
-        resultEl.classList.remove('error');
-        resultEl.classList.add(inputEl.value % 4 === 0 ? 'success' : 'error');
-        inputEl.value = '';
-    }
+  if (isNaN(inputEl.value) || inputEl.value === '') {
+    resultEl.innerHTML = 'Ви не ввели число';
+    resultEl.classList.remove('success');
+    resultEl.classList.add('error');
+  } else {
+    resultEl.innerHTML =
+      inputEl.value % 4 === 0
+        ? 'Ви народилися у високосний рік!'
+        : 'Ви народилися не у високосний рік!';
+    resultEl.classList.remove('error');
+    resultEl.classList.add(inputEl.value % 4 === 0 ? 'success' : 'error');
+    inputEl.value = '';
+  }
 });
--- src/js/rock_scissors_paper.js
+++ src/js/rock_scissors_paper.js
@@ -1,27 +1,25 @@
-const rock = document.querySelector(".rock_js");
-const scissors = document.querySelector(".scissors_js");
-const paper = document.querySelector(".paper_js");
-const rpsResult = document.querySelector(".rsp_result_js");
-const rpsButton = document.querySelector(".rsp_button_js");
-const rpsScore = document.querySelector(".rsp_score_js");
+const rock = document.querySelector('.rock_js');
+const scissors = document.querySelector('.scissors_js');
+const paper = document.querySelector('.paper_js');
+const rpsResult = document.querySelector('.rsp_result_js');
+const rpsButton = document.querySelector('.rsp_button_js');
+const rpsScore = document.querySelector('.rsp_score_js');
 
-//console.log(rock, scissors, paper, rpsResult, rpsButton, rpsScore);
-
-const choices = ["Камінь", "Ножиці", "Папір"];
+const choices = ['Камінь', 'Ножиці', 'Папір'];
 
 let userScore = 0;
 let computerScore = 0;
 
-rock.addEventListener("click", () => {
-  playGame("Камінь");
+rock.addEventListener('click', () => {
+  playGame('Камінь');
 });
 
-scissors.addEventListener("click", () => {
-  playGame("Ножиці");
+scissors.addEventListener('click', () => {
+  playGame('Ножиці');
 });
 
-paper.addEventListener("click", () => {
-  playGame("Папір");
+paper.addEventListener('click', () => {
+  playGame('Папір');
 });
 
 function playGame(userChoice) {
@@ -31,19 +29,19 @@ function playGame(userChoice) {
   rpsButton.textContent = computerChoice;
 
   if (userChoice === computerChoice) {
-    rpsResult.textContent = "Нічия!";
+    rpsResult.textContent = 'Нічия!';
     rpsResult.style.color = '#000000';
   } else if (
-    (userChoice === "Камінь" && computerChoice === "Ножиці") ||
-    (userChoice === "Ножиці" && computerChoice === "Папір") ||
-    (userChoice === "Папір" && computerChoice === "Камінь")
+    (userChoice === 'Камінь' && computerChoice === 'Ножиці') ||
+    (userChoice === 'Ножиці' && computerChoice === 'Папір') ||
+    (userChoice === 'Папір' && computerChoice === 'Камінь')
   ) {
-    rpsResult.textContent = "Ви виграли раунд!";
-    rpsResult.style.color = '#039900';
+    rpsResult.textContent = 'Ви виграли раунд!';
+    rpsResult.style.color = '#2ec660';
     userScore++;
   } else {
     rpsResult.textContent = "Комп'ютер виграв раунд!";
-    rpsResult.style.color = '#990000';
+    rpsResult.style.color = 'rgb(216, 58, 58)';
     computerScore++;
   }
 
